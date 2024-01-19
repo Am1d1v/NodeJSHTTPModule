b717176bf81ad4170bc7be76803732fa4d1176da
Am1d1v <dimadiman1adima@gmail.com>
2024-01-19 16:03:41 +0300

    NodeJS HTTP Module. Multi Paths

--- index.js
+++ index.js
@@ -5,9 +5,20 @@ const PORT = 1000;
 // Create Server
 const server = http.createServer((req, res) => {
     //console.log(res.statusCode);
-    res.setHeader('Content-Type', 'text/html');
-    res.write(`<h1>Status Code: ${res.statusCode} </h1>`);
-    res.end();
+    //console.log(req);
+
+    if(req.url === '/http'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write(`<h1>Status Code: ${res.statusCode} </h1>`);
+        res.end();
+    }
+
+    if(req.url !== '/http'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write(`<h1> Wrong URL </h1>`);
+        res.end();
+    }
+    
 });
 
 server.listen(PORT, () => {
