90817b85c834aa0e7129e3b42dd40cc9172287fa
Am1d1v <dimadiman1adima@gmail.com>
2024-01-19 15:25:03 +0300

    NodeJS HTTP Module

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const http = require('http');
 
+const PORT = 1000;
+
 // Create Server
 const server = http.createServer((req, res) => {
     //console.log(res.statusCode);
@@ -8,4 +10,6 @@ const server = http.createServer((req, res) => {
     res.end();
 });
 
-server.listen(1000);
+server.listen(PORT, () => {
+    console.log(`Server was launched on port ${PORT} `);
+});
