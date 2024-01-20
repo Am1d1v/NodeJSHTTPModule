73a3786f3fa0fe9e3bd368bdf57a70b07b0ed1b1
Am1d1v <dimadiman1adima@gmail.com>
2024-01-20 13:58:03 +0300

    NodeJS HTTP Module refactored

--- index.js
+++ index.js
@@ -20,40 +20,55 @@ const comments = [
     },
 ];
 
+const getHTML = (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write(`<h1>Status Code: ${res.statusCode} </h1>`);
+    return res.end();
+}; 
+
+const getText = (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write(`<p>Some Text</p>`);
+    return res.end();
+}; 
+
+const getComments = (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    return res.end(JSON.stringify(comments));
+}; 
+
+const wrongURL = (req, res) => {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/html');
+    res.write(`<h1> Wrong URL </h1>`);
+    return res.end();
+}; 
+
+
 // Create Server
 const server = http.createServer((req, res) => {
-    //console.log(res.statusCode);
-    //console.log(req);
 
     // Main Page URL-Path
     if(req.url === '/main' || req.url === '/'){
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.write(`<h1>Status Code: ${res.statusCode} </h1>`);
-        return res.end();
+        return getHTML(req, res);
     }
 
     // Text Page URL-Path
-    if(req.url === '/text'){
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.write(`<p>Some Text</p>`);
-        return res.end();
+    if(req.method === 'GET' && req.url === '/text'){
+        return getText(req, res);
     }
 
     // JSON URL-Path
-    if(req.url === '/jsonapp'){
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'applicatiob/json');
-        return res.end(JSON.stringify(comments));
+    if(req.method === 'GET' && req.url === '/comments'){
+        return getComments(req, res);
     }
 
     // Wrong URL Warning and statusCode 404
     if(req.url !== '/main'){
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/html');
-        res.write(`<h1> Wrong URL </h1>`);
-        return res.end();
+        wrongURL(req, res);
     }
     
 });
